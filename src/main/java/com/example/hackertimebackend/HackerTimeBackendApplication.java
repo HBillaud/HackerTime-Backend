e180c551be50abdd2845633eaeaa7d724f511eb6
HBillaud <hpcbillaud@gmail.com>
2022-02-01 16:04:46 -0500

    Initial commit - Gradle Project setup using IntelliJ built-in Spring Initializr feature

---
+++ src/main/java/com/example/hackertimebackend/HackerTimeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackertimebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackerTimeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackerTimeBackendApplication.class, args);
+    }
+
+}

781a626faf29db91bb1160f870dd989d3ec9fa82
HBillaud <hpcbillaud@gmail.com>
2022-04-12 11:59:55 -0400

    return array of reports as part of GET user

--- src/main/java/com/example/hackertimebackend/user/UserServiceImpl.java
+++ src/main/java/com/example/hackertimebackend/user/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.hackertimebackend.user;
 import com.example.hackertimebackend.commons.UserResponse;
 import com.example.hackertimebackend.db.repositories.UserRepository;
 import com.example.hackertimebackend.mapper.UserMapper;
+import com.example.hackertimebackend.report.ReportService;
 import com.example.hackertimebackend.utils.JwtUtils;
 import lombok.RequiredArgsConstructor;
 import org.bson.types.ObjectId;
@@ -15,13 +16,19 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
     @Autowired
+    private ReportService reportService;
+    @Autowired
     private UserMapper userMapper;
     @Autowired
     private JwtUtils jwtUtils;
 
     public UserResponse getUser(String token) throws Exception {
         return userRepository.findById(jwtUtils.getUserNameFromJwtToken(token.substring(7))).map(
-                User -> userMapper.userToUserResponse(User)
+                User -> {
+                    UserResponse res = userMapper.userToUserResponse(User);
+                    res.setReports(reportService.getReports(User.getReportIds()));
+                    return res;
+                }
         ).orElseThrow(
                 () -> new Exception(String.format("User with email %s not found!", token))
         );

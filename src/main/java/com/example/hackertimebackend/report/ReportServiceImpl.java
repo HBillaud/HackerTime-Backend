bac3b9b02e9a564904975fbe0246702c3ed9357e
HBillaud <hpcbillaud@gmail.com>
2022-04-26 20:43:01 -0400

    fix report document to include stdout

--- src/main/java/com/example/hackertimebackend/report/ReportServiceImpl.java
+++ src/main/java/com/example/hackertimebackend/report/ReportServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.hackertimebackend.report;
 
+import com.example.hackertimebackend.WebSocket.CodeStruct;
+import com.example.hackertimebackend.WebSocket.RoomEndpoint;
 import com.example.hackertimebackend.commons.ReportRequest;
 import com.example.hackertimebackend.db.models.Report;
 import com.example.hackertimebackend.db.repositories.ReportRepository;
@@ -10,19 +12,26 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class ReportServiceImpl implements ReportService {
     @Autowired
     private ReportRepository reportRepository;
+    @Autowired
+    private RoomEndpoint roomEndpoint;
 
     public Report generateReport(ReportRequest request) throws Exception {
         try {
+            CodeStruct codeStruct = new CodeStruct("c", request.getCode());
+            Map<String, String> map = roomEndpoint.compile(codeStruct);
+
             Report report = Report.builder()
                     .id(new ObjectId())
                     .question(request.getQuestion())
                     .code(request.getCode())
+                    .output(map.get("stdout"))
                     .intervieweeName("")
                     .createdDate(new Date())
                     .build();
